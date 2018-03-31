0d9fad0a1034e857ecb26e0466a5ab7013c1d2d5
Max Lyashuk <m_lyashuk@ukr.net>
2018-03-31 15:49:39 +0300

    chore: add release note generation script

---
+++ scripts/gen-release-note.js
@@ -0,0 +1,22 @@
+/* eslint-disable strict,no-console,import/no-extraneous-dependencies */
+
+'use strict';
+
+const cc = require('conventional-changelog');
+const fs = require('fs');
+
+const version = process.argv[2] || process.env.VERSION;
+const file = `./RELEASE_NOTE${version ? `_${version}` : ''}.md`;
+const fileStream = fs.createWriteStream(file);
+
+cc({
+  preset: 'angular',
+  pkg: {
+    transform(pkg) {
+      return { ...pkg, version: `v${version}` };
+    },
+  },
+}).pipe(fileStream)
+  .on('close', () => {
+    console.log(`Generated release note at ${file}`);
+  });
