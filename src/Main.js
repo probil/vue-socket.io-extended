fced95f4cd1d9ea8351608aeb4af1b38a292141a
Max Lyashuk <m_lyashuk@ukr.net>
2018-04-04 00:40:06 +0300

    chore: change error prefix when no connection passed

--- src/Main.js
+++ src/Main.js
@@ -4,7 +4,7 @@ import mixin from './mixin';
 
 export default {
   install(Vue, connection, store) {
-    if (!connection) throw new Error('[Vue-Socket.io] cannot locate connection');
+    if (!connection) throw new Error('[vue-socket.io-ext] cannot locate connection');
     const observer = new Observer(connection, store);
     Vue.prototype.$socket = observer.Socket;
     Vue.mixin(mixin(GlobalEmitter));
