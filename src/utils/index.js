a81f1a095638d1e3ab1d9b2eae8fb91d8665749b
Max Lyashuk <m_lyashuk@ukr.net>
2018-04-23 23:30:35 +0300

    feat(utils): add `unwrapIfSingle` helper + tests
    The idea behind `unwrapIfSingle` is to return first array item if it contains only one array item, otherwise return original data.

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,3 +6,9 @@ export const eventToAction = event => `socket_${camelcase(event.replace('SOCKET'
 export const isFunction = obj => typeof obj === 'function';
 
 export const isSocketIo = obj => !!obj && isFunction(obj.on) && isFunction(obj.emit);
+
+export const unwrapIfSingle = args => (
+  args && args.length <= 1
+    ? args[0]
+    : args
+);
