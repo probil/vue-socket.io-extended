e4c224a3822ab8a6e7506f4366be71573b065b8c
Max Lyashuk <m_lyashuk@ukr.net>
2018-05-19 00:49:14 +0300

    refactor: rewrite `passToStore` with FP techniques, extract transformers
    - improves maintainability (`passToStore` is much easier to read)
    - better separation of concerns
    - prepare `actionPrefix` and `mutationPrefix` for moving to options
    - prepare `eventTo{Action|Mutation}Transformer` for moving to options
    - `passToStore` now accepts raw events (no `SOCKET_` shenanigans)
    - slightly improve performance for stores with large amount of actions / mutations

--- src/utils/__tests__/index.spec.js
+++ src/utils/__tests__/index.spec.js
@@ -1,22 +1,4 @@
-import { eventToAction, isFunction, isSocketIo, unwrapIfSingle } from '../';
-
-describe('.eventToAction()', () => {
-  it('should be a function', () => {
-    expect(eventToAction).toEqual(expect.any(Function));
-  });
-
-  it('should correctly convert socket event name to camelcased action name', () => {
-    expect(eventToAction('SOCKET_USER_MESSAGE')).toBe('socket_userMessage');
-    expect(eventToAction('SOCKET_userMessage')).toBe('socket_userMessage');
-    expect(eventToAction('SOCKET_UserMessage')).toBe('socket_userMessage');
-    expect(eventToAction('SOCKET_user-message')).toBe('socket_userMessage');
-  });
-
-  it('should add `socket_` prefix to action', () => {
-    expect(eventToAction('message')).toBe('socket_message');
-    expect(eventToAction('USER_MESSAGE')).toBe('socket_userMessage');
-  });
-});
+import { isFunction, isSocketIo, unwrapIfSingle } from '../';
 
 describe('.isFunction()', () => {
   it('should return false for value other than function', () => {
