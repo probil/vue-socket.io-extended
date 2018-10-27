9c5ee6ab442a98d2b787c260b27c8e0b2f716dfc
Max Lyashuk <m_lyashuk@ukr.net>
2018-10-27 15:41:32 +0300

    feat: typescript config, add type defs to package.json

--- types/vue.d.ts
+++ types/vue.d.ts
@@ -2,18 +2,18 @@
  * Extends interfaces in Vue.js
  */
 
-import Vue, { ComponentOptions } from "vue";
+import _Vue from 'vue';
 import * as SocketIOClient from 'socket.io-client';
 
-declare module "vue/types/options" {
-  interface ComponentOptions<V extends Vue> {
+declare module 'vue/types/options' {
+  interface ComponentOptions<V extends _Vue> {
     sockets?: {
       [keys: string]: Function,
     }
   }
 }
 
-declare module "vue/types/vue" {
+declare module 'vue/types/vue' {
   interface Vue {
     $socket: SocketIOClient.Socket;
   }
