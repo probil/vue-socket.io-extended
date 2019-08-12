ceaedc70fd7d69cb09e209ba9c1fb0472b7bdb01
Max Lyashuk <m_lyashuk@ukr.net>
2019-08-13 01:47:28 +0300

    feat: add reactive `disconnected`, refactor a bit

--- src/Main.js
+++ src/Main.js
@@ -4,38 +4,69 @@ import createMixin from './createMixin';
 import { isSocketIo } from './utils';
 import defaults from './defaults';
 
+/**
+ * @param {Vue} Vue
+ * @param {SocketIOClient} socket
+ * @param {Object} obj
+ * @return {Object}
+ */
+function defineReactiveProperties(Vue, socket, obj) {
+  const configStore = new Vue({
+    data: () => ({
+      connected: false,
+    }),
+    computed: {
+      disconnected() {
+        return !this.connected;
+      },
+    },
+  });
+  socket.on('connect', () => {
+    configStore.connected = true;
+  });
+  socket.on('disconnect', () => {
+    configStore.connected = false;
+  });
+
+  return Object.defineProperties(obj, {
+    connected: {
+      get() {
+        return configStore.connected;
+      },
+      enumerable: false,
+    },
+    disconnected: {
+      get() {
+        return configStore.disconnected;
+      },
+      enumerable: false,
+    },
+  });
+}
+
+/**
+ * @param {SocketIOClient} socket
+ * @param {Object} obj
+ * @return {Object}
+ */
+function defineSocketIoClient(socket, obj) {
+  return Object.defineProperties(obj, {
+    client: {
+      value: socket,
+      writable: false,
+      enumerable: false,
+    },
+  });
+}
+
 export default {
   install(Vue, socket, options) {
     if (!isSocketIo(socket)) {
       throw new Error('[vue-socket.io-ext] you have to pass `socket.io-client` instance to the plugin');
     }
-
     const $socket = {};
-    const configStore = new Vue({
-      data: () => ({
-        connected: false,
-      }),
-    });
-    socket.on('connect', () => {
-      configStore.connected = true;
-    });
-    socket.on('disconnect', () => {
-      configStore.connected = false;
-    });
-
-    Object.defineProperties($socket, {
-      client: {
-        value: socket,
-        writable: false,
-        enumerable: false,
-      },
-      connected: {
-        get() {
-          return configStore.connected;
-        },
-        enumerable: false,
-      },
-    });
+    defineReactiveProperties(Vue, socket, $socket)
+    defineSocketIoClient(socket, $socket)
 
     // eslint-disable-next-line no-param-reassign
     Vue.prototype.$socket = $socket;
