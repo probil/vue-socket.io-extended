97288135c1b6809bf749f7c90c2700278bca1894
Max Lyashuk <m_lyashuk@ukr.net>
2019-08-13 02:12:37 +0300

    refactor: get rid of useless Main.js file

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -1,10 +1,99 @@
-import index from '../index';
-import Main from '../Main';
+import { createLocalVue, mount } from '@vue/test-utils';
+import Main from '../index';
+import io from '../__mocks__/socket.io-client';
 
-it('passes this canary test', () => {
-  expect(true).toBe(true);
+it('should be vue plugin (is an object with `install` method)', () => {
+  expect(Main).toMatchObject({
+    install: expect.any(Function),
+  });
 });
 
-it('should re-export `Main` module', () => {
-  expect(index).toBe(Main);
+describe('.install()', () => {
+  let Vue;
+  beforeEach(() => {
+    Vue = createLocalVue();
+  });
+
+  it('should accept 3 arguments', () => {
+    expect(Main.install).toHaveProperty('length', 3);
+  });
+
+  it('should throw an error when no second argument passed', () => {
+    expect(() => Vue.use(Main))
+      .toThrowErrorMatchingSnapshot();
+  });
+
+  it('should throw an error when second argument is not socket.io instance', () => {
+    expect(() => Vue.use(Main, { a: 1 }))
+      .toThrowErrorMatchingSnapshot();
+    expect(() => Vue.use(Main, 'ws://localhost'))
+      .toThrowErrorMatchingSnapshot();
+  });
+
+  it('should not throw an error when second argument is socket.io instance', () => {
+    const socket = io('ws://localhost');
+    expect(() => Vue.use(Main, socket))
+      .not
+      .toThrow();
+  });
+
+  it('defines socket.io instance as `$socket.client` on Vue prototype', () => {
+    Vue.use(Main, io('ws://localhost'));
+    const wrapper = mount({ render: () => null }, { localVue: Vue });
+    expect(wrapper.vm.$socket.client).toBeDefined();
+    expect(wrapper.vm.$socket.client).toEqual(expect.any(Object));
+    expect(wrapper.vm.$socket.client.on).toEqual(expect.any(Function));
+    expect(wrapper.vm.$socket.client.emit).toEqual(expect.any(Function));
+  });
+
+  it('registers mixin on Vue', () => {
+    const spy = jest.spyOn(Vue, 'mixin');
+    Vue.use(Main, io('ws://localhost'));
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(expect.any(Object));
+  });
+
+  it('correctly merges `sockets` property of the mixin and the component', () => {
+    Vue.use(Main, io('ws://localhost'));
+    const mixinListener = jest.fn();
+    const componentListener = jest.fn();
+    const someMixin = {
+      sockets: { mixinListener },
+    };
+    const wrapper = mount({
+      mixins: [someMixin],
+      render: () => null,
+      sockets: { componentListener },
+    }, { localVue: Vue });
+
+    expect(wrapper.vm.$options.sockets).toMatchObject({
+      mixinListener,
+      componentListener,
+    });
+  });
+});
+
+describe('.defaults', () => {
+  it('is an object', () => {
+    expect(Main.defaults).toEqual(expect.any(Object));
+  });
+
+  it('contains default plugin options', () => {
+    expect(Main.defaults).toMatchObject({
+      actionPrefix: expect.any(String),
+      mutationPrefix: expect.any(String),
+      eventToMutationTransformer: expect.any(Function),
+      eventToActionTransformer: expect.any(Function),
+    });
+  });
+
+  it('all values are read-only', () => {
+    Object.keys(Main.defaults)
+      .forEach((prop) => {
+        const fn = () => {
+          Main.defaults[prop] = 'newValue';
+        };
+        expect(fn).toThrow('Cannot assign to read only property');
+      });
+  });
 });
